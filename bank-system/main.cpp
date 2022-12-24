d086642eab14b48d2afe4fe4cdbf2d58906d717c
zwe <zwezeya02@gmail.com>
2022-12-24 21:08:04 +0800

    Initial Commit

---
+++ bank-system/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bank-system
+//
+//  Created by Zwe Zeya on 24/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
